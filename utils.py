a74561961610cbd936e82d64b3534cc94214718f
Théo Gobinet <theo.gobinet@gmail.com>
2020-10-13 18:25:27 +0200

    Add Rabin-Miller to utils

--- utils.py
+++ utils.py
@@ -117,3 +117,34 @@ def exp_mod(a,exp,mod):
         a=(a*a)%mod
     
     return res
+
+def millerT(n):
+    # Initialisation -> 2^0*d=n-1
+    d = n - 1
+    c = 0
+
+    while d%2 == 0:
+        d = d/2
+        # c factors of 2
+        c = c + 1
+
+    import random as r
+    import math as m
+
+    a = r.randint(2, n-2)
+    x = exp_mod(a,d,n)
+
+    if(x == 1 or x == n - 1):
+        return False
+    else:
+        for i in range(0,c):
+            x = m.pow(x,2) % n
+            if(x == n - 1):
+                return False
+        return True
+    
+def millerR (n, s):
+    for i in range(1, s):
+        if millerT(n):
+            return False
+    return True
