e81f233605345b987b3f1e71bcde079ed549cff0
Théo Gobinet <theo.gobinet@gmail.com>
2020-10-14 21:29:21 +0200

    Added division into block

--- text.py
+++ text.py
@@ -1,3 +1,5 @@
+import math
+
 def strToBinStr(str):
     '''Return string as a binary string'''
     hexString = str.encode('utf_8','ignore').hex()
@@ -19,3 +21,20 @@ def binStrToStr(bin):
     bytes = [bin[x*8:(x+1)*8] for x in range (0, round ((len(bin) / 8)))]
 
     return b''.join([int(x,2).to_bytes(1, byteorder='big') for x in bytes]).decode()
+
+def binArrToBlocArr (arr, size=64):
+    '''Return array divised by blocks of size bits'''
+    if size % 8 != 0 or size==0:
+        print ('Size must be a non null multiple of 8')
+    else:
+        blockSize = int(size / 8)
+
+        if len(arr) % blockSize != 0:
+            for x in range (blockSize - (len(arr) % blockSize)):
+                arr.append("00000000")
+
+        return [arr[x*blockSize:(x+1)*blockSize] for x in range (0, math.ceil(len(arr)/blockSize))]
+
+def blocArrToBinArr (arr):
+    '''Return blocks of bits array concatenated into one array'''
+    return [x for i in arr for x in i]
